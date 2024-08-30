38c43e1aeeb6242448232ba3ed29972acb6cb77d
Aneesh Sharma <aneeshsharma1024@gmail.com>
2024-08-30 10:00:23 +0530

    Fix : Postgres URI correction

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     """Base Configuration"""
-    SQLALCHEMY_DATABASE_URI = os.getenv('POSTGRES_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_ENV = 'development'
     
